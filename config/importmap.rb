c83ae195d8024638676d67f719afb1bc847e1628
Guillaume CABANEL <guillaume@kuartz.fr>
2023-09-07 10:31:48 +0200

    fix importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 after_bundle do
-  insert_into_file "config/importmap.rb", after: 'pin_all_from "app/javascript/controllers", under: "controllers"\n'do
-    <<~RUBY
-      pin_all_from "app/javascript/components", under: "components"
-    RUBY
-  end
+  append_to_file "config/importmap.rb", 'pin_all_from "app/javascript/components", under: "components"'
 end
