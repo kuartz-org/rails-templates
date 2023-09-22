401bcfa3b73b0e2ab55d830ed77af605b9497725
Guillaume CABANEL <guillaume@kuartz.fr>
2023-09-22 17:52:02 +0200

    remove chart js controller

--- _addons/chart_js/template.rb
+++ _addons/chart_js/template.rb
@@ -4,5 +4,3 @@ run "importmap pin 'chart.js'"
 run "importmap pin 'date-fns'"
 run "importmap pin 'date-fns/locale'"
 run "importmap pin 'chartjs-adapter-date-fns'"
-
-template "javascript/controllers/chart_controller.js.tt", "app/javascript/controllers/chart_controller.js"
