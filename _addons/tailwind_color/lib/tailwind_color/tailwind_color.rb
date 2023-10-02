601a0d73860ee50f45013add9d0e20b168d993d7
Guillaume CABANEL <guillaume@kuartz.fr>
2023-10-02 10:27:40 +0200

    add doc for TailwindColor helper

--- _addons/tailwind_color/lib/tailwind_color/tailwind_color.rb
+++ _addons/tailwind_color/lib/tailwind_color/tailwind_color.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+# Available colors: https://tailwindcss.com/docs/customizing-colors#default-color-palette
+# Usage:
+# TailwindColor[:blue, 500].to_hex => "#3b82f6"
 class TailwindColor
   COLORS = YAML.load_file(Rails.root.join("lib", "tailwind_color", "tailwind_colors.yml")).freeze
+  DEFAULT_NUMBER = 500
 
-  def initialize(color, number = 500)
+  def self.[](color, number = DEFAULT_NUMBER)
+    new(color, number)
+  end
+
+  def initialize(color, number = DEFAULT_NUMBER)
     @color = color
     @number = number
   end
